ecf61378ffcca9b1b581fb86268bfdcd6c240c44
Jacob Motta <motta_jake@Yahoo.com>
2025-02-06 14:54:00 -0800

    Tailwind styling tests

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+      'primary': '#1a73e8',
+      'secondary': '#4285f4',
+        // You can also use objects for shade variations
+        'brand': {
+          100: '#e6f0ff',
+          200: '#99c2ff',
+          300: '#66a3ff',
+          400: '#3385ff',
+          500: '#0066ff', // base
+          600: '#0052cc',
+          700: '#003d99',
+        },
+      },
+    },
   },
   plugins: [],
 };
